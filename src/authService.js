bfd6d69e0417381a8c485b160f8bf83d7f358445
Tamir Hen <51208520+TamirHen@users.noreply.github.com>
2022-02-05 12:18:08 -0800

    Auth api call changed from axios to fetch

--- src/authService.js
+++ src/authService.js
@@ -1,31 +1,51 @@
-const axios = require('axios');
-const qs = require('qs');
-
+const fetch = require('node-fetch');
+const {Headers} = fetch;
 
 const authenticate = async () => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
 
-    const data = qs.stringify({
-        'username': process.env.USERNAME,
-        'password': process.env.PASSWORD,
-        'grant_type': 'password'
-    });
-    const config = {
-        method: 'post',
-        url: `${process.env.API_URL}/${process.env.ACCOUNT_NAME}/oauth2/token`,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: data
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("username", process.env.USERNAME);
+    urlencoded.append("password", process.env.PASSWORD);
+    urlencoded.append("grant_type", "password");
+
+    const requestOptions = {
+        method: 'POST',
+        headers,
+        body: urlencoded,
+        redirect: 'follow'
     };
+
     let response
     try {
-        response = await axios(config)
+        response = await fetch(`${process.env.API_URL}/${process.env.ACCOUNT_NAME}/oauth2/token`, requestOptions)
     } catch (error) {
         throw error
     }
-    return response.data
+    return await response.json()
 }
 
 module.exports = {
     authenticate,
 }
+// const data = qs.stringify({
+//     'username': process.env.USERNAME,
+//     'password': process.env.PASSWORD,
+//     'grant_type': 'password'
+// });
+// const config = {
+//     method: 'post',
+//     url: `${process.env.API_URL}/${process.env.ACCOUNT_NAME}/oauth2/token`,
+//     headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//     },
+//     data: data
+// };
+// let response
+// try {
+//     response = await axios(config)
+// } catch (error) {
+//     throw error
+// }
+// return response.data
